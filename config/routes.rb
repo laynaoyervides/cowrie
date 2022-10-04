464f8265a732675806d09d4af8707bf4bfedbee0
Layna Oyervides <laynaoyervides@gmail.com>
2022-10-04 06:59:21 -0600

    rails

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  Rails.application.routes.draw do
+    # route to test your configuration
+    get '/hello', to: 'application#hello_world'
+  end
+end
