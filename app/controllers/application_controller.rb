66beadb50541ac0a052314a2352d23bc60e9d2a8
Layna Oyervides <laynaoyervides@gmail.com>
2022-10-04 21:22:22 -0600

    authorize current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-    def hello_world
-        session[:count] = (session[:count] || 0) + 1
-        render json: { count: session[:count] }
-    end
+         def hello_world
+             session[:count] = (session[:count] || 0) + 1
+            render json: { count: session[:count] }
+        end
+    private
+        def authorize
+        @current_user = User.find_by(id: session[:user_id])
+        render json: {errors: ["Not authorized"]}, status: :unauthorized unless @current_user
+        end
+
+        def render_unprocessable_entity_response(exception)
+        render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+        end
+
 end
