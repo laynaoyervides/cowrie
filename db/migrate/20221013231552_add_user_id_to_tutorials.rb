7d957955bf2d73037c0237b22507d361efe51d2b
Layna Oyervides <laynaoyervides@gmail.com>
2022-10-13 17:16:28 -0600

    user_id to Tutorials

---
+++ db/migrate/20221013231552_add_user_id_to_tutorials.rb
@@ -0,0 +1,5 @@
+class AddUserIdToTutorials < ActiveRecord::Migration[7.0]
+  def change
+    add_column :tutorials, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_231207) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_231552) do
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_231207) do
     t.string "topic"
     t.string "video_url"
     t.string "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
