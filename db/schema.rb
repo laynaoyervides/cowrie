9681baa863c5ab4c2a7743f490b370fea7f65e85
Layna Oyervides <laynaoyervides@gmail.com>
2022-10-13 17:21:36 -0600

    user_id to nfts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_231941) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_232045) do
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_231941) do
   create_table "nfts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "tutorials", force: :cascade do |t|
