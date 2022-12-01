d92ddab172c6a5945a05836dec3754f75e2dc0fe
Layna Oyervides <laynaoyervides@gmail.com>
2022-12-01 11:13:07 -0700

    updating models

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,9 @@
-import { useState, useEffect } from "react";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
-
+  
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
+    <div>
     </div>
   );
 }
