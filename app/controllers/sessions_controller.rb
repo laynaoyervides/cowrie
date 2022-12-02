01bd1ab81d17e113ccde53e212b380afa55b9542
Layna Oyervides <laynaoyervides@gmail.com>
2022-12-01 19:07:29 -0700

    updating sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,18 @@
 class SessionsController < ApplicationController
-    skip_before_action :authorize, only:create
+    skip_before_action :confirm_authentication
     
     def create
-      user = User.find_by(username: params[:username])
+      user = User.find_by_username(params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      render json: user
+      render json: user, status: :ok
     else
       render json: {errors: ["Invalid username or password"]}, status: :unauthorized
     end
   end
   
   def destroy
-    session.delete :user_id
-    head :no_content
+    session.delete(:user_id)
   end
   
   end
