6f90151a687baa64ec722518d5f841956be4cc57
Layna Oyervides <laynaoyervides@gmail.com>
2022-12-18 19:20:18 -0700

    fetching tutorials

--- client/src/Learn.js
+++ client/src/Learn.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tutorials from './Tutorials';
+import {Box} from '@mui/material'
 
-function Learn(props) {
+function Learn() {
+
+const [tutorials, setTutorials] = useState([])
+
+useEffect(() => {
+        fetch(`/api/tutorials`)
+        .then((r)=> r.json())
+        .then((tutorials)=> setTutorials(tutorials))
+},[]
+);
+    
+console.log(tutorials)
+
     return (
         <div>
-            
+            <Box>
+            {tutorials.map((tutorial) => (
+
+                <Tutorials 
+                    key={tutorial.id}
+                    title={tutorial.title}
+                    topic={tutorial.topic}
+                    video_url={tutorial.video_url}
+                    description={tutorial.description}
+                
+                />
+                ))
+            }
+          </Box>
         </div>
     );
 }
