00aba0439cd520fdd2fa1478317a410799a00320
Layna Oyervides <laynaoyervides@gmail.com>
2022-12-18 22:10:33 -0700

    adding delete tutorial

--- client/src/Teach.js
+++ client/src/Teach.js
@@ -1,17 +1,44 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import TeachTutorials from './TeachTutorials';
 
 function Teach({user}) {
 
-const [tutorials, setTutorials] = useEffect([])
+const [tutorials, setTutorials] = useState([])
+
 useEffect (
     () => {
         fetch("/api/tutorials")
         .then((r)=> r.json())
         .then((tutorials)=> setTutorials(tutorials))
-    }
+    }, []
 )
+
+function deleteTutorial (id) {
+    fetch(`/api/tutorials/${id}` , {
+        method: "DELETE",
+    })
+    .then((r)=> {
+   
+        if (r.ok) {
+            setTutorials(tutorials.filter((tutorial)=>tutorial.id !==id));
+        }
+    })
+        .catch((err)=> console.log(err))
+
+};
+
     return (
         <div>
+            {tutorials.map((tutorial)=>
+            <TeachTutorials 
+                title={tutorial.title}
+                topic={tutorial.topic}
+                description={tutorial.description}
+                video_url={tutorial.video_url}
+                cloudinary_public_id={tutorial.cloudinary_public_id}
+                deleteTutorial={deleteTutorial}
+            />
+            )}
         </div>
     );
 }
