493b8aad263108958e5d999f37c32e01aa18ff1c
Layna Oyervides <laynaoyervides@gmail.com>
2022-12-18 19:21:20 -0700

    tutorial view

--- client/src/Tutorials.js
+++ client/src/Tutorials.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Paper, Typography} from '@mui/material'
 
-function Tutorials(props) {
+function Tutorials({title, topic, video_url, description}) {
     return (
-        <div>
-            
-        </div>
+        <>
+        <Paper>
+            <Typography>{title}</Typography>
+            <Typography>{topic}</Typography>
+            <video width="100%" height="100%" controls>
+                <source src={video_url} type="video/mp4"/>
+            </video>
+            <Typography>{description }</Typography>
+
+            </Paper>
+        </>
     );
 }
 
