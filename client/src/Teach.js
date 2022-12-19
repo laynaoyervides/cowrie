cddb99bbc418056ada0a2d2fdf0232ae8d35effb
Layna Oyervides <laynaoyervides@gmail.com>
2022-12-18 19:20:59 -0700

    teach fetch tutorials

--- client/src/Teach.js
+++ client/src/Teach.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-function Teach(props) {
+function Teach({user}) {
+
+const [tutorials, setTutorials] = useEffect([])
+useEffect (
+    () => {
+        fetch("/api/tutorials")
+        .then((r)=> r.json())
+        .then((tutorials)=> setTutorials(tutorials))
+    }
+)
     return (
         <div>
-            
         </div>
     );
 }
