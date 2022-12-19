ff1c124fd99c5b2bdde10b3318be42c3a8c02029
Layna Oyervides <laynaoyervides@gmail.com>
2022-12-18 19:22:12 -0700

    update collection

---
+++ client/src/ViewCollection.js
@@ -0,0 +1,45 @@
+import { Box, Button } from '@mui/material';
+import React, { useState , useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import CollectionDetail from './CollectionDetail';
+
+function ViewCollection() {
+    const {id} = useParams();
+    const [collection, setCollection]= useState([])
+
+   
+    useEffect ( () => {
+
+        fetch(`/api/collections/${id}`)
+        .then((r) => r.json())
+        .then((collection) => setCollection(collection))
+        
+    }, [id]);
+   
+   
+
+      function handleUpdateCollection(updatedCollection){
+        
+        setCollection(updatedCollection);
+    }
+
+
+    return (
+        <div>
+            <Box>
+            <CollectionDetail 
+                            key={collection.id}
+                            title= {collection.title}
+                             description={collection.description}
+                             collection_img = {collection.collection_img}
+                                onUpdateCollection={handleUpdateCollection}
+                                collection ={collection}
+                                setCollection={setCollection}
+                        />    
+                <Button>View Artworks</Button>        
+                </Box>
+        </div>
+    );
+}
+
+export default ViewCollection;
